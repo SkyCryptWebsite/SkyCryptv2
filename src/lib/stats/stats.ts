4a8179bd32c080baa11b722aaccba6a4261a6075
Zickles <zicklesistaken@gmail.com>
2024-06-11 16:03:29 -0400

    fix: structure and move files that import server to server folder

--- src/lib/stats/stats.ts
+++ src/lib/stats/stats.ts
@@ -3,7 +3,7 @@ export * from "$lib/stats/rank";
 export * from "$lib/stats/skyblock_level";
 export * from "$lib/stats/main_stats";
 export * from "$lib/stats/items";
-export * from "$lib/stats/accessories";
+export * from "$lib/server/stats/accessories";
 export * from "$lib/stats/pets";
 export * from "$lib/stats/mining";
 export * from "$lib/stats/farming";
