80aad5a26b41505a80644b11ceb76ff709e4caba
Zickles <zicklesistaken@gmail.com>
2024-06-11 15:54:21 -0400

    fix: update-items imports

--- src/lib/server/constants/update-items.ts
+++ src/lib/server/constants/update-items.ts
@@ -1,6 +1,6 @@
 import type { DatabaseItem } from "$types/global";
-import { ITEMS } from "./items";
-import MONGO from "../server/db/mongo";
+import { ITEMS } from "$lib/constants/items";
+import MONGO from "../../server/db/mongo";
 
 async function updateItems() {
   const items = await MONGO.collection("items").findOne({});
