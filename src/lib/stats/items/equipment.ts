7b22c75a8a4b09d8fcde64fa2026f7900a2ffa3c
Zickles <zicklesistaken@gmail.com>
2024-06-17 18:36:58 -0400

    fix(equipment): reverse equipment

--- src/lib/stats/items/equipment.ts
+++ src/lib/stats/items/equipment.ts
@@ -5,14 +5,14 @@ import * as helper from "$lib/helper";
 export function getEquipment(equipment: ProcessedItem[]) {
   if (equipment.some((a) => helper.getId(a) === "") === true) {
     return {
-      equipment: equipment.filter((a) => helper.getId(a) !== ""),
+      equipment: equipment.filter((a) => helper.getId(a) !== "").reverse(),
       stats: {}
     };
   }
   // TODO: implement name & rarity parsing just like the armor
 
   return {
-    equipment,
+    equipment: equipment.reverse(),
     stats: getStatsFromItems(equipment)
   };
 }
