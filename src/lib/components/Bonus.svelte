390d278edd6c6a468cb53746b4802d24d0eb8c02
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-06-19 16:03:25 +0200

    feat: add bonus component

---
+++ src/lib/components/Bonus.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { STATS_DATA } from "$constants/stats";
+  import { cn } from "$lib/utils";
+
+  export let stats;
+  export let title = "Bonus:";
+
+  let classNames: string | undefined = undefined;
+  export { classNames as class };
+
+  const statsData = Object.entries(stats);
+</script>
+
+<p class={cn("space-x-0.5 font-bold capitalize leading-6 text-text/60", classNames)}>
+  <span>{title}</span>
+  {#each statsData as [key, value], index}
+    <span class={STATS_DATA[key].color}>
+      {value}{STATS_DATA[key].suffix}
+      {STATS_DATA[key].nameTiny}
+    </span>
+    {#if statsData.length - 1 !== index}
+      // {" "}
+    {/if}
+  {/each}
+</p>
