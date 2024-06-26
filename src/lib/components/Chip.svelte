08318c6b4da22f3623c33c0f03def81287860a97
Gigi <47110839+DarthGigi@users.noreply.github.com>
2024-06-26 23:36:49 +0200

    feat: Update Chip component with tooltip variant

--- src/lib/components/Chip.svelte
+++ src/lib/components/Chip.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { cn } from "$lib/utils";
-  import { Avatar } from "bits-ui";
+  import { cn, flyAndScale } from "$lib/utils";
+  import { Avatar, Tooltip } from "bits-ui";
   import Image from "lucide-svelte/icons/image";
   import { fade } from "svelte/transition";
 
@@ -21,20 +21,32 @@
     class?: string;
   };
 
+  export let variant: "default" | "tooltip" = "default";
   export let animationOptions: AnimationOptions = { animate: false };
   export let image: ImageProps;
 
   let classNames = "";
   export { classNames as class };
+
+  const isTooltip = variant === "tooltip";
 </script>
 
-<div class={cn("flex w-full max-w-60 items-center gap-2 rounded-lg bg-background/30 p-2", classNames)} in:fade|global={{ duration: animationOptions.animate ? 300 : 0, delay: animationOptions.animate ? 25 * (animationOptions.index + 1) : 0 }} out:fade|global={{ duration: animationOptions.animate ? 300 : 0, delay: animationOptions.animate ? 25 * (animationOptions.amountOfItems - animationOptions.index) : 0 }}>
-  <Avatar.Root>
-    <Avatar.Image src={image.src} class={cn("aspect-square size-12 object-contain", image.class)} />
-    <Avatar.Fallback>
-      <Image class="size-10" />
-    </Avatar.Fallback>
-  </Avatar.Root>
-
-  <slot />
-</div>
+<Tooltip.Root group="chip" openDelay={0} closeDelay={0}>
+  <Tooltip.Trigger asChild let:builder>
+    <div class={cn("flex w-full max-w-60 items-center gap-2 rounded-lg bg-background/30 p-2", classNames)} use:builder.action {...builder} in:fade|global={{ duration: animationOptions.animate ? 300 : 0, delay: animationOptions.animate ? 25 * (animationOptions.index + 1) : 0 }} out:fade|global={{ duration: animationOptions.animate ? 300 : 0, delay: animationOptions.animate ? 25 * (animationOptions.amountOfItems - animationOptions.index) : 0 }}>
+      <Avatar.Root>
+        <Avatar.Image src={image.src} class={cn("aspect-square size-12 object-contain", image.class)} />
+        <Avatar.Fallback>
+          <Image class="size-10" />
+        </Avatar.Fallback>
+      </Avatar.Root>
+      <slot />
+    </div>
+  </Tooltip.Trigger>
+  {#if isTooltip}
+    <Tooltip.Content class="z-50 rounded-lg bg-background-grey p-4" transition={flyAndScale} transitionConfig={{ y: 8, duration: 150 }} sideOffset={6} side="top" align="center">
+      <slot name="tooltip" />
+      <Tooltip.Arrow />
+    </Tooltip.Content>
+  {/if}
+</Tooltip.Root>
