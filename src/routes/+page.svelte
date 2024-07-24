d0e148c648ddc74e69bc013b582ca18c55c63716
Zickles <zicklesistaken@gmail.com>
2024-07-24 15:31:01 -0400

    feat(homepage): add search func

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
-  import Header from "$lib/components/Header.svelte";
+  import { goto } from "$app/navigation";
+  import type { ButtonEvents } from "bits-ui";
   import { Button } from "bits-ui";
-</script>
 
-<Header />
+  var ign: string = "";
+
+  function goToProfile(event: ButtonEvents["click"]) {
+    event.preventDefault();
+
+    if (ign !== "") {
+      goto("/stats/" + ign);
+    }
+  }
+</script>
 
 <main class="mx-auto flex h-full max-w-[68rem] flex-col justify-center gap-6 pb-[max(1.25rem+env(safe-area-inset-bottom))] pl-[max(1.25rem+env(safe-area-inset-left))] pr-[max(1.25rem+env(safe-area-inset-right))] pt-5">
   <form class="flex w-full flex-col justify-center gap-6 rounded-lg py-6 text-3xl backdrop-blur-lg backdrop-brightness-50">
     <h2 class="m-1 w-full text-center font-semibold">Show SkyBlock stats for</h2>
-    <input type="search" name="ign" enterkeyhint="go" required placeholder="Enter username" class="relative h-16 flex-grow bg-text/10 text-center font-normal text-text focus-visible:outline-none" />
-    <Button.Root type="submit" class="mx-auto flex w-full max-w-fit items-center justify-center rounded-3xl bg-icon px-6 py-3 text-base font-bold uppercase transition-all duration-150 [text-shadow:0_0_3px_rgba(0,0,0,.5)] hover:scale-[1.015]">Show me</Button.Root>
+    <input type="search" bind:value={ign} name="ign" enterkeyhint="go" required placeholder="Enter username" class="relative h-16 flex-grow bg-text/10 text-center font-normal text-text focus-visible:outline-none" />
+    <Button.Root type="submit" on:click={goToProfile} class="mx-auto flex w-full max-w-fit items-center justify-center rounded-3xl bg-icon px-6 py-3 text-base font-bold uppercase transition-all duration-150 [text-shadow:0_0_3px_rgba(0,0,0,.5)] hover:scale-[1.015]">Show me</Button.Root>
   </form>
   <Button.Root href="https://discord.gg/cNgADv2kEQ" target="_blank" rel="noreferrer" class="block rounded-lg p-4 backdrop-blur-lg backdrop-brightness-50 transition-all duration-150 hover:scale-[1.015]">
     <div class="font-semibold">
