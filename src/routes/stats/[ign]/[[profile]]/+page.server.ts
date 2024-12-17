85a6b72db8a9377ff7fcef29d7ed891d5521533e
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2024-12-17 23:09:57 +0100

    fix(stats): update API endpoint to use relative path for fetching data

--- src/routes/stats/[ign]/[[profile]]/+page.server.ts
+++ src/routes/stats/[ign]/[[profile]]/+page.server.ts
@@ -1,11 +1,9 @@
 import type { PageServerLoad } from "./$types";
 
-const baseAPI = "http://localhost:5173/api/stats";
-
 export const load = (async ({ params, fetch }) => {
   const { ign, profile } = params;
 
-  const data = fetch(`${baseAPI}/${ign}/${profile}`)
+  const data = fetch(`/api/stats/${ign}/${profile}`)
     .then((res) => res.json())
     .catch((err) => {
       console.error(err);
