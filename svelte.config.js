82f967ebde44083f7b49ae8e61ae306f3fe49b37
DuckySoLucky <robertkovac160@gmail.com>
2024-12-23 20:10:24 +0100

    merge with main

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from "@sveltejs/adapter-node";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,6 @@ const config = {
   // for more information about preprocessors
 
   preprocess: vitePreprocess({ script: true }),
-
   ssr: {
     noExternal: process.env.NODE_ENV === "production" ? ["@napi-rs/canvas"] : []
   },
