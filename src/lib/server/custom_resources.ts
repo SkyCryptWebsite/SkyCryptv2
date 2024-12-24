2b77650f33b353dcddc4a6309a8d733fd6e51ca0
DuckySoLucky <robertkovac160@gmail.com>
2024-12-24 16:15:11 +0100

    perf(custom-resources): early break, better perf

--- src/lib/server/custom_resources.ts
+++ src/lib/server/custom_resources.ts
@@ -743,6 +743,10 @@ export function getTexture(item: ProcessedItem, { pack_ids = [], hotm = false }:
     if (cachedItemIdTextureMap) {
       outputTexture = processTextures(outputTexture, cachedItemIdTextureMap, pack, item as Item);
     }
+
+    if (outputTexture.path) {
+      break;
+    }
   }
 
   if (!("path" in outputTexture)) {
@@ -756,9 +760,6 @@ export function getTexture(item: ProcessedItem, { pack_ids = [], hotm = false }:
     // 	.replace(/\\/g, '/');
   } else {
     outputTexture.path = "/" + path.relative(path.resolve(FOLDER_PATH, "static"), outputTexture.path as string);
-    if (outputTexture.pack !== undefined) {
-      outputTexture.pack.base_path = "/" + path.relative(path.resolve(FOLDER_PATH, "static"), outputTexture.pack?.base_path as string);
-    }
   }
 
   // debugStats.time_spent_ms = Date.now() - timeStarted;
