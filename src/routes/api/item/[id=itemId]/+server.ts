f82f8f5007ce04e8e53a659289509716fb4c8bc8
DuckySoLucky <robertkovac160@gmail.com>
2024-12-24 14:32:18 +0100

    feat(stats): add packs support

--- src/routes/api/item/[id=itemId]/+server.ts
+++ src/routes/api/item/[id=itemId]/+server.ts
@@ -5,11 +5,9 @@ import type { RequestHandler } from "./$types";
 // GET /api/head/[id=itemId]
 export const GET: RequestHandler = async ({ params }) => {
   const { id } = params;
-  console.log(id);
 
   try {
     const [newId, damage] = id.split(":");
-    console.log(newId, { damage: isNaN(parseInt(damage)) ? 0 : parseInt(damage) });
     const attachment = await renderItem(newId, { damage: isNaN(parseInt(damage)) ? 0 : parseInt(damage) });
 
     return new Response(attachment.image, { headers: { "Content-Type": "image/png" } });
